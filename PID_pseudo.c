81cc72592bb0d5d05d63183ea38ba692a29c2743
KrankedSpartan <intelphysic@gmail.com>
2016-11-23 09:04:02 -0500

    Comments
    Added explanation comments

--- PID_pseudo.c
+++ PID_pseudo.c
@@ -1,10 +1,8 @@
-previous_error = 0
-integral = 0 
-
-  error = setpoint - measured_value
-  integral = integral + error*dt
-  derivative = (error - previous_error)/dt
-  output = Kp*error + Ki*integral + Kd*derivative
-  previous_error = error
-  wait(dt)
-  goto start //while loop
+  error = setpoint - measured_value // calculates the proportional error which is target - actualValue
+  integral = integral + error*dt // Determines the history of the error overtime to minimize undershoot
+  derivative = (error - previous_error)/dt //Trying to preempt the next error to minimize overshoot
+  output = Kp*error + Ki*integral + Kd*derivative // Proportional + Integral + Derivative
+  previous_error = error /* sends the value of the last error into the previous_error private 
+  variable to be used in the derivative calculation */
+  wait(dt) // refresh rate of the PID control loop
+  goto start //a while loop with a never ennding condition could be used 
